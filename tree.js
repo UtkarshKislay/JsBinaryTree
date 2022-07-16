0739169d910b513e96825a2d44f7da1e1d846f69
Utkarsh Kislay <utkarsh2237@gamil.com>
2022-07-17 04:12:52 +0530

    add addValueFunction again

--- tree.js
+++ tree.js
@@ -9,13 +9,13 @@ Tree.prototype.search=function(val){
     var found=this.root.search(val);
     return found;
 }
-// Tree.prototype.addValue=function(val){
-//     var n = new node(val);
-//     if(this.root==null){
-//         this.root=n;
-//         this.root.x=width/2;
-//         this.root.y=20;
-//     }else{
-//         this.root.addNode(n);
-//     }
-// }
+Tree.prototype.addValue=function(val){
+    var n = new node(val);
+    if(this.root==null){
+        this.root=n;
+        this.root.x=width/2;
+        this.root.y=20;
+    }else{
+        this.root.addNode(n);
+    }
+}
